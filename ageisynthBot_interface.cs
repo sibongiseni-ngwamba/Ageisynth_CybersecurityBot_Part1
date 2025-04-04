ecfdd324dd74a3f23f14fc34c2f244ef70c8e7e6
sibongiseni-ngwamba <ST10454956@rcconnect.edu.za>
2025-04-04 16:12:54 +0000

    Added global variables and arraylists

--- ageisynthBot_interface.cs
+++ ageisynthBot_interface.cs
@@ -1,8 +1,18 @@
-﻿namespace Ageisynth_CybersecurityBot_Part1
+﻿using System.Collections;
+
+namespace Ageisynth_CybersecurityBot_Part1
 {
     // Class responsible for chatbot interaction and response handling
     public class ageisynthBot_interface
     {
+        // Global variables to store user name and input
+        private string user_name = string.Empty;
+        private string user_asking = string.Empty;
+
+        // ArrayLists to store possible replies and words to ignore in user input
+        private ArrayList replies = new ArrayList();
+        private ArrayList ignore = new ArrayList();
+
         public ageisynthBot_interface()
         {
         }// End of Constructor
