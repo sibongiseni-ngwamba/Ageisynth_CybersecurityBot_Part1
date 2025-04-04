a554578ceb37f76e684467be1f3cf8051bdb7909
sibongiseni-ngwamba <ST10454956@rcconnect.edu.za>
2025-04-04 14:12:30 +0000

    Inserted comments, changed the access modifier in th main class from internal to public and created an instance for the voice greeting

--- Program.cs
+++ Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Ageisynth_CybersecurityBot_Part1
 {
-    internal class Program
+    // Main Program class that serves as the entry point of the application
+    public class Program
     {
+        // Main method - execution starts here
         static void Main(string[] args)
         {
+            // Create a new instance of the voice_greeting class
+            // This will automatically play the welcome greeting audio
+            new voice_greeting() { };
         }
     }
 }
---
+++ voice_greeting.cs
@@ -0,0 +1,9 @@
+﻿namespace Ageisynth_CybersecurityBot_Part1
+{
+    internal class voice_greeting
+    {
+        public voice_greeting()
+        {
+        }
+    }
+}
